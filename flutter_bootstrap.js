1d29a028cebf74d6d8bbad819fa1296ea8d24f66
Raouf Loukriz <raoufloukriz8@gmail.com>
2026-06-12 16:35:10 +0200

    fix: consistent Emp Rpts card heights (mainAxisExtent), remove Expanded clash

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2701429730" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2170853263" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
